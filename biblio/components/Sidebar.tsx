5b5b01c686281fc60d1bf4dd9cad422efd9b6d23
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-11-07 19:58:43 -0600

    update sidebar styles

--- biblio/components/Sidebar.tsx
+++ biblio/components/Sidebar.tsx
@@ -2,10 +2,10 @@ import { categories } from '@/constants';
 
 export const Sidebar = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-56 shadow-xl text-blue-800 font-semibold">
       {categories.map((category) => {
         return (
-          <button key={category} className="p-2 border-2">
+          <button key={category} className="p-2 text-left hover:bg-blue-100">
             {category}
           </button>
         );
