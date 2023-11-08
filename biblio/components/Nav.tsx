3fa2754a99f4b6346ea78a85185f29b7b4ac6ef9
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-11-07 19:08:14 -0600

    update nav

--- biblio/components/Nav.tsx
+++ biblio/components/Nav.tsx
@@ -1,17 +1,34 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const routes = [
+  { name: 'Inicio', path: '/' },
+  { name: 'Alta', path: '/new' },
+  { name: 'Favoritos', path: '/favorites' },
+];
 
 export const Nav = () => {
+  const path = usePathname();
+
   return (
-    <div className="flex border-2 justify-between p-5">
+    <div className="flex border-2 justify-between py-5 px-14 items-center">
       <span>LOGO</span>
-      <div className="flex gap-5">
-        <Link href="/" className="border-b-2">
-          Inicio
-        </Link>
-        <Link href="/new">Alta</Link>
-        <Link href="/favorites">Favoritos</Link>
-      </div>
-      <p>Luis Orlando Ortega</p>
+      <ul className="flex gap-5">
+        {routes.map((route) => (
+          <li
+            key={route.name}
+            className={`w-36 p-2 justify-center flex font-bold ${
+              path === route.path
+                ? 'border-b-2 border-green-300 text-blue-900'
+                : 'text-gray-400'
+            }`}
+          >
+            <Link href={route.path}>{route.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <p className="font-bold text-blue-900">Luis Orlando Ortega</p>
     </div>
   );
 };
