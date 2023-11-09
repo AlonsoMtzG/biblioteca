8a801cf2d8bed06e589192ea6338b011c9494fee
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-11-09 13:19:40 -0600

    update book register use in /new page

--- biblio/app/new/page.tsx
+++ biblio/app/new/page.tsx
@@ -1,10 +1,25 @@
+'use client';
 import { BookRegister } from '@/components/BookRegister';
-import { Sidebar } from '@/components/Sidebar';
+import { DataContext } from '@/context/DataProvider';
+import { Book } from '@/interfaces';
+import { useRouter } from 'next/navigation';
+import { useContext } from 'react';
 
 export default function New() {
+  const router = useRouter();
+  const { addBook } = useContext(DataContext);
+
+  const handleAddBook = (data: Book) => {
+    addBook(data);
+  };
+
   return (
     <div className="w-full h-full flex justify-center">
-      <BookRegister />
+      <BookRegister
+        saveAction="add"
+        onSubmit={handleAddBook}
+        onClose={() => router.push('/')}
+      />
     </div>
   );
 }
